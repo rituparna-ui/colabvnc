9cdecb74c763441d314ba05dc8126ad8c579c909
RITUPARNA <rwarwatkar@gmail.com>
2020-07-22 21:33:51 +0530

    Init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "colabvnc.py",
+    version = "0.1",
+    py_modules = ['colabvnc'],
+    url = "https://github.com/rituparna-ui/colabvnc",
+    author = "Rituparna Warwatkar",
+)
